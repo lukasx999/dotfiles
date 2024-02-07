047b0c50babc644620179c88dc52138da7fa6bdb
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2024-02-07 16:00:23 +0100

    added awesome wm

---
+++ suckless/dwm-flexipatch/dwm-flexipatch/themes/catppuccin_macchiato.h
@@ -0,0 +1,40 @@
+static char normfgcolor[]                = "#b7bdf8";
+static char normbgcolor[]                = "#363a4f";
+static char normbordercolor[]            = "#363a4f"; //#444444
+static char normfloatcolor[]             = "#db8fd9";
+
+static char selfgcolor[]                 = "#8aadf4";
+static char selbgcolor[]                 = "#494d64";
+static char selbordercolor[]             = "#8aadf4";
+static char selfloatcolor[]              = "#494d64"; //#494d64
+
+static char titlenormfgcolor[]           = "#b7bdf8"; //#d8dee9
+static char titlenormbgcolor[]           = "#363a4f"; //#2e3440 - #000000
+static char titlenormbordercolor[]       = "#444444"; //#444444
+static char titlenormfloatcolor[]        = "#db8fd9";
+
+static char titleselfgcolor[]            = "#8aadf4"; //#81a1c1
+static char titleselbgcolor[]            = "#494d64"; //#3b4252
+static char titleselbordercolor[]        = "#494d64"; //#3b4252
+static char titleselfloatcolor[]         = "#494d64";
+
+static char tagsnormfgcolor[]            = "#b7bdf8";
+static char tagsnormbgcolor[]            = "#363a4f";
+static char tagsnormbordercolor[]        = "#444444";
+static char tagsnormfloatcolor[]         = "#db8fd9";
+
+static char tagsselfgcolor[]             = "#8aadf4";
+static char tagsselbgcolor[]             = "#494d64";
+static char tagsselbordercolor[]         = "#494d64";
+static char tagsselfloatcolor[]          = "#494d64";
+
+static char hidnormfgcolor[]             = "#b7bdf8"; //#3b3252
+static char hidselfgcolor[]              = "#8aadf4"; //#227799
+static char hidnormbgcolor[]             = "#363a4f"; //#2e3440
+static char hidselbgcolor[]              = "#494d64"; //#2e3440
+
+static char urgfgcolor[]                 = "#b7bdf8"; //#d8dee9
+static char urgbgcolor[]                 = "#363a4f"; //#363a4f
+static char urgbordercolor[]             = "#ff0000";
+static char urgfloatcolor[]              = "#db8fd9";
+
---
+++ suckless/dwm-flexipatch/dwm-flexipatch/themes/nord.h
@@ -0,0 +1,40 @@
+static char normfgcolor[]                = "#d8dee9";
+static char normbgcolor[]                = "#2e3440";
+static char normbordercolor[]            = "#2e3440"; //#444444
+static char normfloatcolor[]             = "#db8fd9";
+
+static char selfgcolor[]                 = "#81a1c1";
+static char selbgcolor[]                 = "#3b4252";
+static char selbordercolor[]             = "#81a1c1";
+static char selfloatcolor[]              = "#3b4252";
+
+static char titlenormfgcolor[]           = "#d8dee9"; //#d8dee9
+static char titlenormbgcolor[]           = "#2e3440"; //#2e3440 - #000000
+static char titlenormbordercolor[]       = "#444444"; //#444444
+static char titlenormfloatcolor[]        = "#db8fd9";
+
+static char titleselfgcolor[]            = "#81a1c1"; //#81a1c1
+static char titleselbgcolor[]            = "#3b4252"; //#3b4252
+static char titleselbordercolor[]        = "#3b4252"; //#3b4252
+static char titleselfloatcolor[]         = "#3b4252";
+
+static char tagsnormfgcolor[]            = "#d8dee9";
+static char tagsnormbgcolor[]            = "#2e3440";
+static char tagsnormbordercolor[]        = "#444444";
+static char tagsnormfloatcolor[]         = "#db8fd9";
+
+static char tagsselfgcolor[]             = "#81a1c1";
+static char tagsselbgcolor[]             = "#3b4252";
+static char tagsselbordercolor[]         = "#3b4252";
+static char tagsselfloatcolor[]          = "#3b4252";
+
+static char hidnormfgcolor[]             = "#d8dee9"; //#3b3252
+static char hidselfgcolor[]              = "#81a1c1"; //#227799
+static char hidnormbgcolor[]             = "#2e3440"; //#2e3440
+static char hidselbgcolor[]              = "#3b4252"; //#2e3440
+
+static char urgfgcolor[]                 = "#d8dee9"; //#d8dee9
+static char urgbgcolor[]                 = "#2e3440"; //#2e3440
+static char urgbordercolor[]             = "#ff0000";
+static char urgfloatcolor[]              = "#db8fd9";
+
